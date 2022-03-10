79601b7ad295fc76626bfeeb4267152bc3f13d73
Jason Rogers <d3lo.jr@gmail.com>
2022-03-09 19:58:40 -0500

    Add output textarea

--- app/javascript/packs/screen.jsx
+++ app/javascript/packs/screen.jsx
@@ -3,28 +3,47 @@ import Keyboard from 'react-simple-keyboard';
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 
-class App extends Component {
+class InputBoard extends Component {
+  state = { input: "" }
+
   onChange = (input) => {
     console.log("Input changed", input);
+    this.setState({ input: input });
   }
 
   onKeyPress = (button) => {
+    if(button == "{enter}") {
+      // Send this.state.input to backend
+      // reset input
+    }
     console.log("Button pressed", button);
   }
 
-  render(){
+  render() {
     return (
-      <Keyboard
-        onChange={this.onChange}
-        onKeyPress={this.onKeyPress}
-      />
+      <>
+        <Keyboard
+          onChange={this.onChange}
+          onKeyPress={this.onKeyPress}
+          layout={{
+            default: ["! Q W E R T Y U I O P {bksp}", "A S D F G H J K L {enter}", "Z X C V B N M , .", "{space}"], // "{bksp}"
+            shift: ["! Q W E R T Y U I O P {bksp}", "A S D F G H J K L {enter}", "Z X C V B N M , .", "{space}"]
+          }}
+        />
+        <textarea
+          className="pi-screen-textarea"
+          readOnly
+          rows="1"
+          value={this.state.input}
+        />
+      </>
     );
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <InputBoard />,
     document.body.appendChild(document.createElement('div')),
   )
 })
